7ae304b979fa9aebc3987688d85da65e458281f7
Alejandro E. Rendon <alejandro@rendon.co>
2023-07-23 15:59:39 -0500

    Migrar archivos estaticos

--- static/js/app.js
+++ static/js/app.js
@@ -3,3 +3,10 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Hello World');
 });
+
+let map = L.map('map').setView([51.505, -0.09], 2);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
