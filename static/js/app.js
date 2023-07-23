a0a6af61949fc1bcfb327e616a36c2f52fc0711e
Alejandro E. Rendon <alejandro@rendon.co>
2023-07-23 16:20:26 -0500

    Corregir errores de formato

--- static/js/app.js
+++ static/js/app.js
@@ -4,7 +4,8 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('Hello World');
 });
 
-let map = L.map('map').setView([51.505, -0.09], 2);
+const L = window.L;
+const map = L.map('map').setView([51.505, -0.09], 2);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
