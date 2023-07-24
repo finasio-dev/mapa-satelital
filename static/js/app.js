fa2988fd5e7149fc9f0cad5dd18fa3b3625ace97
Alejandro E. Rendon <alejandro@rendon.co>
2023-07-23 20:09:53 -0500

    Obtener datos iniciales al renderizar la vista

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,30 @@
-// Custom scripts
+// Get initial data from API
 
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('Hello World');
+const GET_ALL_SATELLITES_URL = 'https://api.tinygs.com/v1/satellites';
+// const GET_SATELLITE_URL = 'https://api.tinygs.com/v1/satellite/{id}';
+
+document.addEventListener('DOMContentLoaded', async () => {
+  const satellites = await getAllSatellites();
+
+  console.log(satellites);
 });
+
+// Get all satellites from API
+const getAllSatellites = async () => {
+  const satellites = await fetch(GET_ALL_SATELLITES_URL)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error('Request failed!');
+    })
+    .catch((error) => {
+      console.warn(error);
+    });
+  return satellites;
+};
+
+// Leaflet map
 
 const L = window.L;
 const map = L.map('map').setView([51.505, -0.09], 2);
