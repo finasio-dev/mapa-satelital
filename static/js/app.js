bf806ec225bc756d433eb2adf528349966655094
juribedio <juribedio@hotmail.com>
2023-07-29 18:41:49 -0500

    obtener satélite mediante id

--- static/js/app.js
+++ static/js/app.js
@@ -1,20 +1,56 @@
+// Get initial data from API
+
+const GET_ALL_SATELLITES_URL = 'https://api.tinygs.com/v1/satellites';
+const GET_SATELLITE_URL = 'https://api.tinygs.com/v1/satellite/';
+
 $(document).ready(async () => {
-  // Get initial data from API
   const satellites = await getAllSatellites();
 
-  // Este bloque recorre la lista de satélites e inserta en el dropdown el id
+  //Este bloque recorre la lista de satélites e inserta en el dropdown el id
   satellites.map((satelite) => {
-    $('#lista-satelites').append(
-      `<li><a class="dropdown-item satelite" href="#" id="${satelite.name}">${satelite.displayName}</a></li>`
-    );
-    return satelite;
-  });
-  // Eventos
-  $('.satelite').on('click', (event) => {
-    console.log(event.target.id);
-    // (... rest of your JS code)
-  });
+    $("#lista-satelites").append(`<li><a class="dropdown-item satelite" href="#" id="${satelite.name}">${satelite.displayName}</a></li>`);
+  })
+  //Eventos
+  $(".satelite").on('click', async(event) =>{
+    const satelliteById = event.target.id;
+    //encontrar datos del satélite mediante función getSatelliteById
+    const dataSatellite = await getSatelliteById(satelliteById);
+    const tle = dataSatellite.tle;
+
 });
+
+
+});
+
+// Get all satellites from API
+const getAllSatellites = async () => {
+  const satellites = await fetch(GET_ALL_SATELLITES_URL)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error('Request failed!');
+    })
+    .catch((error) => {
+      console.warn(error);
+    });
+  return satellites;
+};
+//get a satellites by id
+
+const getSatelliteById = async (id) => {
+  const satellite = await fetch(`${GET_SATELLITE_URL}${id}`)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error('Request failed!');
+    })
+    .catch((error) => {
+      console.warn(error);
+    });
+  return satellite;
+};
 
 // Leaflet map
 
